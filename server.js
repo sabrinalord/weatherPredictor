3247967270d1628537047c5a09f117f02cb37ec2
Sabrina <sabrina@msn.com>
2023-09-16 19:13:01 +0100

    update server url

--- server.js
+++ server.js
@@ -39,5 +39,9 @@ app.get("/coordinates", async (req, res) => {
   }
 });
 
+const port = 8000 || process.env.REACT_APP_BACKEND_URL
 
-app.listen(8000, '0.0.0.0', () => console.log(`Server is running`))
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
