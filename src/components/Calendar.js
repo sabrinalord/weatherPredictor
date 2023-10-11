62c7e94f27aea0226adf712b299b5d5642450df3
Sabrina Lord <sabrinalord@Sabrinas-MacBook-Air.local>
2023-10-11 09:50:28 +0100

    fix for yyyy-mm not working in safari or ios

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -15,10 +15,11 @@ const Calendar = ( {month, year, averages, weatherDataRangeInYears, location, we
   const getDaysArrayForMonth = (year, month) => {
     try{    
       console.log('getting days of the month')
-      const daysInMonth = 30;
+      const fixDateForAllBrowsers = dateString => dateString.replace(/-/g, '/');
+      const fixedDateString = fixDateForAllBrowsers(dateString);
+      const daysInMonth = new Date(fixedDateString + '-01').getDate();
       const array = Array.from({ length: daysInMonth }, (_, index) => index + 1);
-      console.log('Days retrieved:', array); 
-      console.log('Days retrieved:', array); 
+      console.log('Days in month:', daysInMonth); 
       
       return array;
     } catch (error) {
